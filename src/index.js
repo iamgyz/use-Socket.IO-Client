2cfb2e61e30f1d8c39d3bf51c98b151059a04b9c
Jake Morales <jmora060@ucr.edu>
2020-04-03 02:23:18 -0700

    fixed initial null problem

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 export const useSocket = (...args) => {
-  const [socket, setSocket] = useState(null);
+  const [socket] = useState(() => io(...args));
 
   useEffect(() => {
-    setSocket(io(...args));
 
     return () => {
       socket && socket.removeAllListeners();
